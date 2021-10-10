593a18f2017cfa5d17265d45fd3cb66451f82856
jlucas1224 <joao.lucas.cordeiro61@aluno.ifce.edu.br>
2021-10-10 15:30:04 -0300

    Site concluído
    Sujeito a mudanças

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-function esconderAreas(){
-	document.getElementById("inicio").style.display = "none"
-	document.getElementById("eu").style.display = "none"
-	document.getElementById("sobreMim").style.display = "none"
-	document.getElementById("formação").style.display = "none"
-	document.getElementById("cursos").style.display = "none"
-	document.getElementById("experiência").style.display = "none"
-	document.getElementById("habilidades").style.display = "none"
+function detalhesJogo() {
+    document.getElementById("jogo-info").style.display = "inline-block"
 }
-function inicio(){
-	esconderAreas()
-	document.getElementById("inicio").style.display = "inline-block"
-	document.getElementById("eu").style.display = "inline-block"
-}
-function sobreMim(){
-	esconderAreas()
-	document.getElementById("sobreMim").style.display = "inline-block"
-}
-function formacao(){
-	esconderAreas()
-	document.getElementById("formação").style.display = "inline-block"
-}
-function cursos(){
-	esconderAreas()
-	document.getElementById("cursos").style.display = "inline-block"
-}
-function experiencia(){
-	esconderAreas()
-	document.getElementById("experiência").style.display = "inline-block"
-}
-function habilidades(){
-	esconderAreas()
-	document.getElementById("habilidades").style.display = "inline-block"
-}
+function esconderDetalhesJogo() {
+    document.getElementById("jogo-info").style.display = "none"
+} 
